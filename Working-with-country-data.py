04a349a6a43d05cc1eb09eaf8c38ec8730b4b478
AlexMclean42 <113861200+AlexMclean42@users.noreply.github.com>
2023-01-03 18:07:59 -0700

    Update Working-with-country-data.py

--- Working-with-country-data.py
+++ Working-with-country-data.py
@@ -1,5 +1,5 @@
-#Final_Project_ENDG233_Fall2021.py
-#ALEXANDER MCLEAN, DOMINICO MENDES, ENDG 233 F21
+
+#ALEXANDER MCLEAN, DOMINICO MENDES, F21
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -25,7 +25,7 @@ class CountryData:
 def main():
     
     # Intro print statement
-    print("ENDG 233 Country Data\n")
+    print("Working with Country Data\n")
     
     # Lists of countries, statistics, years, country independant statistics
     list_of_countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium', 'Belize','Benin','Bhutan','Bolivia', 'Bosnia and Herzegovina','Botswana','Brazil','Brunei','Bulgaria','Burkina Faso','Burundi','Cambodia','Cameroon','Canada','Cape Verde','Central African Republic','Chad','Chile','China','Colombia','Comoros','Costa Rica','Cote d\'Ivoire','Croatia','Cuba','Cyprus','Czech Republic','Dem. Rep. of Congo','Denmark','Djibouti','Dominica','Dominican Republic','Ecuador','Egypt','El Salvador','Equatorial Guinea','Eritrea', 'Estonia', 'Eswatini','Ethiopia','Fed. Sts. Micronesia','Fiji','Finland','France','Gabon','Gambia','Georgia','Germany','Ghana','Greece','Grenada','Guatemala','Guinea','Guinea-Bissau','Guyana','Haiti','Honduras','Hungary','Iceland','India','Indonesia','Iran','Iraq','Ireland','Israel','Italy','Jamaica','Japan','Jordan','Kazakhstan','Kenya','Kiribati','Kuwait','Kyrgyz Republic','Lao','Latvia','Lebanon','Lesotho','Liberia','Libya','Liechtenstein','Lithuania','Luxembourg','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands','Mauritania','Mauritius','Mexico','Moldova','Monaco','Mongolia','Montenegro','Morocco','Mozambique','Myanmar','Namibia','Nauru','Nepal','Netherlands','New Zealand','Nicaragua','Niger','Nigeria','North Korea','North Macedonia','Norway','Oman','Pakistan','Palau','Palestine','Panama','P\'apua New Guinea','Paraguay','Peru','Philippines','Poland','Portugal','Qatar','Rep. of Congo','Romania','Russia','Rwanda','Samoa','San Marino','Sao Tome and Principe','Saudi Arabia','Senegal','Serbia','Seychelles','Sierra Leone','Singapore','Slovak Republic','Slovenia','Solomon Islands','Somalia','South Africa','South Korea','South Sudan','Spain','Sri Lanka','St. Kitts and Nevis','St. Lucia','St. Vincent and the Grenadines','Sudan','Suriname','Sweden','Switzerland','Syria','Tajikistan','Tanzania','Thailand','Timor-Leste','Togo','Tonga','Trinidad and Tobago','Tunisia','Turkey','Turkmenistan','Tuvalu','Uganda','Ukraine','United Arab Emirates','United Kingdom','United States','Uruguay','Uzbekistan','Vanuatu','Venezuela','Vietnam','Yemen','Zambia','Zimbabwe']
